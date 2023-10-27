b6338151932a6525ccb4413554018624f2847277
Naoki0509 <hmcn3176naoki8086@gmail.com>
2023-10-27 23:16:10 +0900

    fix:firebase admin.

---
+++ src/libs/firebase/admin.ts
@@ -0,0 +1,22 @@
+import admin from 'firebase-admin'
+
+export const serviceAccount: admin.ServiceAccount = {
+  projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+  privateKey: (process.env.FIREBASE_ADMIN_PRIVATE_KEY ?? '').replace(
+    /\\n/g,
+    '\n',
+  ),
+  clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+}
+
+export const firebaseAdmin =
+  admin.apps[0] ||
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  })
+
+if (!serviceAccount) {
+  throw new Error('Invalid service account')
+} else {
+  console.log('Firebase Admin Initialized')
+}
