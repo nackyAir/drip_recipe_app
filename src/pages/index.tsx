f26839efebba48d71f1f3a0691b9f6520552f17b
Naoki0509 <hmcn3176naoki8086@gmail.com>
2023-12-07 19:53:55 +0900

    fix: if of router.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import Router from 'next/router'
 
 const Home = () => {
-  if (process.env.NODE_ENV === 'production') {
-    return Router.push('/user')
-  }
+  if (typeof window !== 'undefined') Router.push('/user')
 
   return (
     <>
