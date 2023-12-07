049ce5e7af70cd1fdbe9622498a48f196dfad25f
Naoki0509 <hmcn3176naoki8086@gmail.com>
2023-12-07 19:50:32 +0900

    fix: add title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import Router from 'next/router'
 
 const Home = () => {
-  if (process.env.NODE_ENV === 'production') Router.push('/user')
+  if (process.env.NODE_ENV === 'production') {
+    return Router.push('/user')
+  }
 
-  return <></>
+  return (
+    <>
+      <h1>TopPage</h1>
+    </>
+  )
 }
 
 export default Home
