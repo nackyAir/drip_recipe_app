178ca786173a7d5e03588877d0d59159548c750a
Naoki0509 <hmcn3176naoki8086@gmail.com>
2023-12-08 10:58:39 +0900

    fea: create user session api directory.

---
+++ src/pages/api/user/session.ts
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { setCookie } from 'nookies'
+
+import { firebaseAdmin } from '~/libs/firebase/admin'
+
+const sessionApi = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'POST')
+    return res.status(404).json({ message: 'Not Found' })
+
+  const auth = firebaseAdmin.auth()
+
+  const expiresIn = 60 * 60 * 24 * 5 * 1000
+  const idToken = (JSON.parse(req.body).id || '').toString()
+
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn,
+  })
+
+  const options = {
+    maxAge: expiresIn,
+    httpOnly: true,
+    secure: true,
+    path: '/',
+  }
+
+  setCookie({ res }, 'session', sessionCookie, options)
+
+  res.end(JSON.stringify({ status: 'success' }))
+}
+
+export default sessionApi
