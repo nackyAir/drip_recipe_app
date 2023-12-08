19d22e9368be9f21b618ee846a17d5b3ebb9220c
Naoki0509 <hmcn3176naoki8086@gmail.com>
2023-12-08 10:54:56 +0900

    fea: create store Layout.

---
+++ src/components/store/Layout/index.tsx
@@ -0,0 +1,71 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { ReactNode } from 'react'
+
+import {
+  AppShell,
+  Avatar,
+  Footer,
+  Group,
+  Header,
+  Title,
+  createStyles,
+} from '@mantine/core'
+
+import { useAuthContext } from '~/libs/user/auth'
+
+export const StoreLayout = ({ children }: { children: ReactNode }) => {
+  const { user } = useAuthContext()
+  const router = useRouter()
+
+  const styles = createStyles((themes) => {
+    return {
+      footer: {
+        backgroundColor: themes.colorScheme === 'dark' ? '#000' : '#fff',
+        color: themes.colorScheme === 'dark' ? '#fff' : '#000',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      header: {
+        backgroundColor: themes.colorScheme === 'dark' ? '#000' : '#fff',
+        color: themes.colorScheme === 'dark' ? '#fff' : '#000',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: '0 1rem',
+      },
+    }
+  })
+
+  const { classes } = styles()
+
+  return (
+    <AppShell
+      header={
+        <Header height={70} className={classes.header}>
+          <Group>
+            <Link href="/user" passHref legacyBehavior>
+              <Title order={3}>Coffee Recipe for Store</Title>
+            </Link>
+          </Group>
+          <Group>
+            <Avatar
+              src={user ? user.photoURL : ''}
+              radius="xl"
+              size={45}
+              onClick={() => router.push('/store/setting')}
+            />
+          </Group>
+        </Header>
+      }
+      footer={
+        <Footer height={50} className={classes.footer}>
+          {new Date().getFullYear()} © Coffee Recipe App
+        </Footer>
+      }
+    >
+      {children}
+    </AppShell>
+  )
+}
