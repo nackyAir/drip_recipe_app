ba7aacaf8052835ed0752b33872b4ab698b0e2a2
Naoki0509 <hmcn3176naoki8086@gmail.com>
2023-12-08 10:59:39 +0900

    fea: create user page directoly.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import 'react-toastify/ReactToastify.css'
 
 import { MantineProvider } from '@mantine/core'
 
-import { AuthProvider } from '~/libs/firebase/auth'
+import { AuthProvider } from '~/libs/user/auth'
 import '~/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
